7e0dd790e3e3758ae07cf5749e3d4d3ff4a5c73d
Marco Maccio <marco.maccio@marmac.name>
2015-09-03 23:24:41 +0200

    Changed the script to point to karma.conf.ci.js

--- scripts/private/test-all.sh
+++ scripts/private/test-all.sh
@@ -25,5 +25,5 @@ node_modules/.bin/http-server -p 8000 &
 WEBSERVER_PID=$!
 
 # Run the unit and e2e tests
-node_modules/karma/bin/karma start src/test/js/karma.conf.js --single-run
+node_modules/karma/bin/karma start src/test/js/karma.conf.ci.js --single-run
 node_modules/.bin/protractor src/test/js/protractor-conf.js
